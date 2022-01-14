b8722f63f82cd9b77f867fe02985a649bb00a851
depthbomb <reesepup77@hotmail.com>
2022-01-13 18:42:25 -0600

    refactor(commands): update `update`

--- src/commands/update.ts
+++ src/commands/update.ts
@@ -1,31 +1,29 @@
 import log from '#logger';
 import { gt } from 'semver';
-import { join } from 'node:path';
 import { request } from '@octokit/request';
-import { readFile } from 'node:fs/promises';
+import { getInstalledVersion } from '#utils';
 
 import type { ICommand } from '#interfaces/ICommand';
-
-async function _getInstalledTag() {
-	const packagePath = join(__dirname, '../', '../', 'package.json');
-	const packageContents = await readFile(packagePath, 'utf8');
-	const packageJSON = JSON.parse(packageContents);
-
-	return packageJSON.version;
-};
 
 export default {
 	name: 'update',
 	description: 'checks for updates to Fur.JS',
 	aliases: ['upd', 'up'],
 	async action() {
-		const installedTag = await _getInstalledTag();
-		const { data: release } = await request('GET /repos/{owner}/{repo}/releases/latest', {
-			owner: 'furjs',
-			repo: 'cli'
-		});
+		log.info('update', 'Checking for new releases');
+		log.silly('update', 'Grabbing installed version from package.json');
+
+		const installedTag = await getInstalledVersion();
+
+		log.silly('update', 'Got version', installedTag);
+		log.silly('update', 'Sending request');
+
+		const { data: release } = await request('GET /repos/{owner}/{repo}/releases/latest', { owner: 'furjs', repo: 'cli' });
+
+		log.silly('update', 'Latest release tag =',  release.tag_name);
+
 		if (gt(release.tag_name, installedTag)) {
-			log.info('update', `A new version of Fur.JS is available to download: ${release.name}`);
+			log.info('update', `A new version of Fur.JS is available to download:`, release.name);
 			if (release.body) {
 				log.info('update', release.body);
 			}
